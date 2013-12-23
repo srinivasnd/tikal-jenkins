2e254c0e8383bca42e2b8400ff19c14e2d9e0042
Haggai Philip Zagury <hagzag@gmail.com>
2013-12-23 23:49:04 +0200

    moving attributes scope

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default['java']['jdk']['version'] = '7'
+default['java']['jdk']['install_flavor'] = "oracle"
+default['java']['oracle']['accept_oracle_download_terms'] = true
+
+default['jenkins']['http_proxy']['variant'] = 'nginx'
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,12 +8,6 @@
 #
 #
 
-# set jave version stuff
-node.set['java']['jdk']['version'] = '7'
-node.set['java']['jdk']['install_flavor'] = "oracle"
-node.set['java']['oracle']['accept_oracle_download_terms'] = true
-
 # set jenkins proxy variant
-node.set['jenkins']['http_proxy']['variant'] = 'nginx'
 include_recipe "jenkins::server"
 include_recipe "jenkins::proxy"
